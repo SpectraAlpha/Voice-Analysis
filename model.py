60e3eb6b924351614fb6977d9288965d23897b87
tniromin <tniromin2@gmail.com>
2025-08-17 01:14:46 +0530

    REFACTOR : Corrected Class names

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 
-class AudioCNN(nn):
+class ResidualBlock(nn):
     def __init__(self, in_channels,out_channels, stride=1):
         super().__init__()
         self.conv1=nn.Conv2d(in_channels,out_channels,3,stride, padding=1, bias=False)
@@ -28,7 +28,7 @@ class AudioCNN(nn):
 
         return out
     
-    class ResidualBlock(nn.Module):
+    class AudioCNN(nn.Module):
         def __init__(self,num,classes=50):
             super().__init__()
             self.conv1=nn.Sequential(
